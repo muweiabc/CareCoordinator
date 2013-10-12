970aa64aa9cef046c7868d1a774de022aed5ddaa
Belmen <belmen@outlook.com>
2013-10-12 14:21:52 -0400

    add dummy fragments

--- src/cis573/carecoor/MainActivity.java
+++ src/cis573/carecoor/MainActivity.java
@@ -6,6 +6,10 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
 
@@ -37,7 +41,7 @@ public class MainActivity extends FragmentActivity {
 
 		@Override
 		public Fragment getItem(int arg0) {
-			return new Fragment();
+			return DummyFragment.newInstance(PAGE_TITLES[arg0]);
 		}
 
 		@Override
@@ -49,5 +53,39 @@ public class MainActivity extends FragmentActivity {
 		public CharSequence getPageTitle(int position) {
 			return PAGE_TITLES[position];
 		}
+	}
+	
+	public static class DummyFragment extends Fragment {
+
+		private String mTitle;
+		
+		public static DummyFragment newInstance(String title) {
+			DummyFragment f = new DummyFragment();
+			Bundle args = new Bundle();
+			args.putString("title", title);
+			f.setArguments(args);
+			return f;
+		}
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			Bundle args = getArguments();
+			if(args != null) {
+				mTitle = args.getString("title");
+			}
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View view = inflater.inflate(R.layout.dummy_fragment, container, false);
+			TextView tv = (TextView) view;
+			if(mTitle != null) {
+				tv.setText(mTitle);
+			}
+			return view;
+		}
+		
 	}
 }

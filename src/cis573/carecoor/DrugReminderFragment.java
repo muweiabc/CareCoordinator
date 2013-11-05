f75dd557dbfdc785fc01045c226582a32c501a02
weimu <weimu@192.168.1.107>
2013-11-04 22:44:15 -0500

    11/4

--- src/cis573/carecoor/DrugReminderFragment.java
+++ src/cis573/carecoor/DrugReminderFragment.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import android.R.integer;
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
@@ -23,6 +24,7 @@ import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -43,45 +45,10 @@ public class DrugReminderFragment extends Fragment
 {
 	private Button addDrug;
 	private Button showDrugs;
-	private TimePickerDialog.OnTimeSetListener cb=new  TimePickerDialog.OnTimeSetListener(){
-
-		@Override
-		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	};
-	private OnClickListener listener=new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			int id=v.getId();
-			if(id==R.id.showdrugs){
-				Intent intent=new Intent(getActivity(), DruglistActivity.class);
-				//startActivityForResult(intent,0);
-				startActivity(intent);
-			}
-			if(id==R.id.addDrug){
-				/*
-				Intent intent = new Intent(DashboardScreen.this, ServiceClass.class);
-				PendingIntent pintent = PendingIntent.getService(DashboardScreen.this, 0, intent, 0);
-				AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-				alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30*1000, pintent);*/
-				
-				TimePickerDialog pick=new TimePickerDialog(getActivity(),cb,1,2,true);
-				pick.show();
-				AlarmManager manager=(AlarmManager) getActivity().getSystemService(getActivity().ALARM_SERVICE);
-				manager.set(AlarmManager.RTC, 10000, null);
-			}
-		}
-	};
-	/*
-	public void display(View view){
-		Intent intent=new Intent(getActivity(), DruglistActivity.class);
-		//startActivityForResult(intent,0);
-		startActivity(intent);
-	}*/
+	private Button addApptButton;
+	private TextView nextapptTextView;
+	private TextView nextrefillTextView;
+	
 	/**
 	 * Mandatory empty constructor for the fragment manager to instantiate the
 	 * fragment (e.g. upon screen orientation changes).
@@ -98,14 +65,69 @@ public class DrugReminderFragment extends Fragment
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.drug_reminder_fragment, container, false);
+		View view = inflater.inflate(R.layout.reminder_fragment, container, false);
 		addDrug=(Button)view.findViewById(R.id.addDrug);
 		showDrugs=(Button)view.findViewById(R.id.showdrugs);
+		addApptButton=(Button)view.findViewById(R.id.addappt);
+		nextapptTextView=(TextView)view.findViewById(R.id.nextappt);
+		nextrefillTextView=(TextView)view.findViewById(R.id.nextrefill);
+		
 		addDrug.setOnClickListener(listener);
-		// Set the adapter
 		showDrugs.setOnClickListener(listener);
+		addApptButton.setOnClickListener(listener);
 		return view;
 	}
+	
+	private TimePickerDialog.OnTimeSetListener cb=new  TimePickerDialog.OnTimeSetListener(){
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {		
+		}		
+	};
+	
+	private DatePickerDialog.OnDateSetListener cb2=new  DatePickerDialog.OnDateSetListener(){
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
+	
+	private OnClickListener listener=new OnClickListener() {		
+		@Override
+		public void onClick(View v) {
+			int id=v.getId();
+			if(id==R.id.showdrugs){
+				Intent intent=new Intent(getActivity(), DruglistActivity.class);
+				//startActivityForResult(intent,0);
+				startActivity(intent);
+			}else
+			if(id==R.id.addDrug){
+				/*
+				Intent intent = new Intent(DashboardScreen.this, ServiceClass.class);
+				PendingIntent pintent = PendingIntent.getService(DashboardScreen.this, 0, intent, 0);
+				AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+				alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30*1000, pintent);*/
+				
+				TimePickerDialog pick=new TimePickerDialog(getActivity(),cb,1,2,true);
+				pick.show();
+				AlarmManager manager=(AlarmManager) getActivity().getSystemService(getActivity().ALARM_SERVICE);
+				manager.set(AlarmManager.RTC, 10000, null);
+			}else 
+			if(id==R.id.addappt){
+				DatePickerDialog datePickerDialog=new DatePickerDialog(getActivity(),cb2,1,1,1);
+				datePickerDialog.show();
+			}
+		}
+	};
+	/*
+	public void display(View view){
+		Intent intent=new Intent(getActivity(), DruglistActivity.class);
+		//startActivityForResult(intent,0);
+		startActivity(intent);
+	}*/
 }
 
 	
